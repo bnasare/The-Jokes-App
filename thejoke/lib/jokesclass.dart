bc7bae39f1224c8173668a7dce2c788d13af9e64
bnasare <124492150+bnasare@users.noreply.github.com>
2023-07-10 21:49:35 +0000

    new update

--- thejoke/lib/jokesclass.dart
+++ thejoke/lib/jokesclass.dart
@@ -4,17 +4,17 @@ class Jokes {
   String setup;
   String punchline;
 
-  Jokes({
-  required this.id,
-  required this.type,
-  required this.punchline,
-  required this.setup});
+  Jokes(
+      {required this.id,
+      required this.type,
+      required this.punchline,
+      required this.setup});
 
-  factory Jokes.fromJson(Map<String, dynamic> jokes){
+  factory Jokes.fromJson(Map<String, dynamic> jokes) {
     return Jokes(
-    id: jokes['id'],
-    type: jokes['type'],
-    punchline: jokes['punchline'],
-    setup: jokes['setup']);
+        id: jokes['id'],
+        type: jokes['type'],
+        punchline: jokes['punchline'],
+        setup: jokes['setup']);
   }
 }
